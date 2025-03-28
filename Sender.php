75addf6a69c264a523f416e750b32ca7e1ed5234
ozanuykun <ozanbote@gmail.com>
2025-03-28 08:54:47 +0300

    Fixed mime check.

--- Sender.php
+++ Sender.php
@@ -662,14 +662,14 @@ class Sender implements SenderInterface
                 $mime = NULL;
             }
         }
-        
-        if( strpos($file, '://') === false && ! file_exists($file) )
-        {
-            $this->error[] = $this->getLang('email:attachmentMissing', $file);
-        }
 
         if( empty($mime) )
         {
+            if( strpos($file, '://') === false && ! file_exists($file) )
+            {
+                $this->error[] = $this->getLang('email:attachmentMissing', $file);
+            }
+
             if( $fp = @fopen($file, 'rb') )
             {
                 $fileContent = stream_get_contents($fp);
